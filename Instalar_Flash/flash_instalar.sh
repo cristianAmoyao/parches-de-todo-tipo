1896b0beec526e4f02ece2b0c38826019717349c
cristianAmoyao <crristian@hotmail.es>
2017-06-08 12:05:00 -0300

    Add files via upload

---
+++ Instalar_Flash/flash_instalar.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+read -p "Primero libpepflashplayer.so : " flash
+read -p "Segundo manifest.json : " flash2
+echo "usted Movera los archivos $flash y $flash2 "
+mkdir /usr/lib/adobe-flashplugin
+echo "cp $flash /usr/lib/adobe-flashplugin;cp $flash2 /usr/lib/adobe-flashplugin;rm move.sh" > move.sh
+
+echo "inciando..."
+sudo sh move.sh
